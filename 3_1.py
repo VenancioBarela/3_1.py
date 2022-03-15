e4a0b7604a77708e80c204b6e776b0173cc8cf83
Venancio Barela <calivee.ca@gmail.com>
2022-03-15 15:19:21 -0700

    second

--- 3_1.py
+++ 3_1.py
@@ -4,7 +4,30 @@ Relational Operator
 logical Operator
 and, or, not, in
 """
-# true_or_false = b > a
-true_or_false = "b" > "a"
+# # true_or_false = b > a
+# true_or_false = "b" > "a"
 
-print("true_or_false", true_or_false)
+# print("true_or_false", true_or_false)
+
+# c = "abc"
+
+# true_or_false not done
+
+
+
+
+
+
+# list1= [6, 4, -5, 3.5]
+
+# print (list1)
+# list1.sort()
+# print (list1)
+
+# list2 = ["ha", "hi", "B", "7"]
+# print (list2)
+
+# list2.sort()
+# print(list2)
+
+
